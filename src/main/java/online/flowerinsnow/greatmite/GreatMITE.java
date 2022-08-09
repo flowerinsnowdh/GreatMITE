ebe39fe5377c93613b2d85bf67d9578673c768a9
flowerinsnow <flowerinsnow@donghua.email.cn>
2022-08-09 21:04:39 +0100

    生成项目

---
+++ src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
@@ -0,0 +1,10 @@
+package online.flowerinsnow.greatmite;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GreatMITE implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
