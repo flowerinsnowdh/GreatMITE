0e7ca7861056e0ffbe2ac93bf3fad62d6572943e
flowerinsnow <flowerinsnow@donghua.email.cn>
2022-08-10 09:11:56 +0100

    尝试修改物品堆叠数量

--- src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
+++ src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
@@ -2,17 +2,32 @@ package online.flowerinsnow.greatmite;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import online.flowerinsnow.greatmite.item.ItemBowlWater;
 import online.flowerinsnow.greatmite.listener.BowlListener;
+import online.flowerinsnow.greatmite.util.ReflectMirror;
 
 public class GreatMITE implements ModInitializer {
     public static final ItemBowlWater BOWL_WATER = new ItemBowlWater();
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("greatmite", "water_bowl"), BOWL_WATER);
+        initMaxCount();
+
+        registerNewItems();
 
         UseItemCallback.EVENT.register(new BowlListener());
+    }
+
+    /**
+     * 修改物品的最大堆叠
+     */
+    private void initMaxCount() {
+        ReflectMirror.Item.setMaxCount(Items.BOWL, 16);
+    }
+
+    private void registerNewItems() {
+        Registry.register(Registry.ITEM, new Identifier("greatmite", "water_bowl"), BOWL_WATER);
     }
 }
--- src/main/java/online/flowerinsnow/greatmite/util/ReflectMirror.java
+++ src/main/java/online/flowerinsnow/greatmite/util/ReflectMirror.java
@@ -7,6 +7,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -15,12 +16,21 @@ public final class ReflectMirror {
     public static class Item {
         public static final Class<net.minecraft.item.Item> CLASS = net.minecraft.item.Item.class;
 
+        public static final Field F_MAXCOUNT = getField(CLASS, "field_8013", "maxCount");
         public static final Method M_RAYCAST = getMethod(CLASS, new Class[]{World.class, PlayerEntity.class, RaycastContext.FluidHandling.class},"method_7872", "raycast");
 
         public static BlockHitResult raycast(World world, PlayerEntity player, RaycastContext.FluidHandling fluidHandling) {
             try {
                 return (BlockHitResult) M_RAYCAST.invoke(null, world, player, fluidHandling);
             } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new UtilException(e);
+            }
+        }
+
+        public static void setMaxCount(net.minecraft.item.Item item, int maxCount) {
+            try {
+                F_MAXCOUNT.set(item, maxCount);
+            } catch (IllegalAccessException e) {
                 throw new UtilException(e);
             }
         }
@@ -50,6 +60,23 @@ public final class ReflectMirror {
         }
 
         return method;
+    }
+
+    public static Field getField(Class<?> cls, String... possibleNames) {
+        Field field = null;
+        for (String name : possibleNames) {
+            try {
+                field = cls.getDeclaredField(name);
+                break;
+            } catch (NoSuchFieldException ignored) {
+            }
+        }
+
+        if (field != null) {
+            field.setAccessible(true);
+        }
+
+        return field;
     }
 
     private ReflectMirror() {
