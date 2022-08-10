e9b96af4462b4435c4649761491111938c039059
flowerinsnow <flowerinsnow@donghua.email.cn>
2022-08-10 15:27:05 +0100

    添加橘子

--- src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
+++ src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
@@ -44,6 +44,8 @@ public class GreatMITE implements ModInitializer {
     public static final Item LEMON = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16));
     public static final Item ONION = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16)
             .food(new FoodComponent.Builder().hunger(1).saturationModifier(0.1F).build()));
+    public static final Item ORANGE = new Item(new FabricItemSettings().group(ItemGroup.FOOD).maxCount(16)
+            .food(new FoodComponent.Builder().hunger(1).saturationModifier(0.2F).build()));
 
     @Override
     public void onInitialize() {
@@ -99,5 +101,6 @@ public class GreatMITE implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier(MODID, "dough"), DOUGH);
         Registry.register(Registry.ITEM, new Identifier(MODID, "lemon"), LEMON);
         Registry.register(Registry.ITEM, new Identifier(MODID, "onion"), ONION);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "orange"), ORANGE);
     }
 }
