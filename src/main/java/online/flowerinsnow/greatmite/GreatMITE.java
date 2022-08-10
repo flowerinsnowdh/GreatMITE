ad48ee5f37bc8621ab20f985313bab45f8a998cb
flowerinsnow <flowerinsnow@donghua.email.cn>
2022-08-10 12:34:27 +0100

    添加了更多原有碗装食物
    牛肉羹
    牛奶碗
    麦片粥
    鸡汤
    奶油蘑菇汤
    奶油蔬菜汤
    冰激凌
    土豆泥
    蓝莓粥
    南瓜粥
    沙拉
    果汁雪糕
    蔬菜汤

--- src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
+++ src/main/java/online/flowerinsnow/greatmite/GreatMITE.java
@@ -6,11 +6,27 @@ import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import online.flowerinsnow.greatmite.item.ItemBowlWater;
+import online.flowerinsnow.greatmite.item.ItemStackableStew;
 import online.flowerinsnow.greatmite.listener.BowlListener;
 import online.flowerinsnow.greatmite.util.ReflectMirror;
 
 public class GreatMITE implements ModInitializer {
+    public static final String MODID = "greatmite";
     public static final ItemBowlWater BOWL_WATER = new ItemBowlWater();
+    public static final ItemStackableStew SALAD = new ItemStackableStew(1, 1.0F);
+    public static final ItemStackableStew BEEF_STEW = new ItemStackableStew(16, 16.0F);
+    public static final ItemStackableStew BOWL_MILK = new ItemStackableStew(1, 0.0F);
+    public static final ItemStackableStew CEREAL = new ItemStackableStew(2, 4.0F);
+    public static final ItemStackableStew CHICKEN_SOUP = new ItemStackableStew(10, 10.0F);
+    public static final ItemStackableStew CREAM_OF_MUSHROOM_SOUP = new ItemStackableStew(5, 3.0F);
+    public static final ItemStackableStew CREAM_OF_VEGETABLE_SOUP = new ItemStackableStew(7, 7.0F);
+    public static final ItemStackableStew ICE_CREAM = new ItemStackableStew(7, 7.0F);
+    public static final ItemStackableStew MASHED_POTATO = new ItemStackableStew(8, 12.0F);
+    public static final ItemStackableStew PORRIDGE = new ItemStackableStew(2, 4.0F);
+    public static final ItemStackableStew PUMPKIN_SOUP = new ItemStackableStew(2, 1.0F);
+    public static final ItemStackableStew SORBET = new ItemStackableStew(2, 4.0F);
+    public static final ItemStackableStew VEGETABLE_SOUP = new ItemStackableStew(6, 6.0F);
+
     @Override
     public void onInitialize() {
         initMaxCount();
@@ -25,9 +41,23 @@ public class GreatMITE implements ModInitializer {
      */
     private void initMaxCount() {
         ReflectMirror.Item.setMaxCount(Items.BOWL, 16);
+        // TODO 更多物品堆叠上限待修改
     }
 
     private void registerNewItems() {
-        Registry.register(Registry.ITEM, new Identifier("greatmite", "water_bowl"), BOWL_WATER);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "water_bowl"), BOWL_WATER);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "salad"), SALAD);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "beef_stew"), BEEF_STEW);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "bowl_milk"), BOWL_MILK);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "cereal"), CEREAL);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "chicken_soup"), CHICKEN_SOUP);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "cream_of_mushroom_soup"), CREAM_OF_MUSHROOM_SOUP);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "cream_of_vegetable_soup"), CREAM_OF_VEGETABLE_SOUP);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "ice_cream"), ICE_CREAM);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "mashed_potato"), MASHED_POTATO);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "porridge"), PORRIDGE);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "pumpkin_soup"), PUMPKIN_SOUP);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "sorbet"), SORBET);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "vegetable_soup"), VEGETABLE_SOUP);
     }
 }
---
+++ src/main/java/online/flowerinsnow/greatmite/item/ItemStackableStew.java
@@ -0,0 +1,56 @@
+package online.flowerinsnow.greatmite.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+import online.flowerinsnow.greatmite.util.InventoryUtils;
+
+/**
+ * 用碗装的食物类
+ */
+public class ItemStackableStew extends Item {
+    /**
+     * 如果此项为true，则该物品的食用方式为喝而不是吃
+     */
+    public final boolean drink;
+
+    public ItemStackableStew(int hunger, float saturation) {
+        this(hunger, saturation, false);
+    }
+
+    public ItemStackableStew(int hunger, float saturation, boolean drink) {
+        super(new FabricItemSettings()
+                .food(new FoodComponent.Builder().hunger(hunger).saturationModifier(saturation).build())
+                .maxCount(4).group(ItemGroup.FOOD));
+        this.drink = drink;
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof ServerPlayerEntity player) {
+            Criteria.CONSUME_ITEM.trigger(player, stack);
+            player.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+
+        if (user instanceof PlayerEntity player && !player.isCreative()) {
+            stack.decrement(1);
+            if (stack.isEmpty()) {
+                return new ItemStack(Items.BOWL);
+            } else {
+                InventoryUtils.addAnEmptyBowl(player.getInventory());
+            }
+        }
+        return stack;
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return drink ? UseAction.DRINK : UseAction.EAT;
+    }
+}
